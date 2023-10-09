1d1ab8b218f9689a5ec3e71a249ca7caeb2552be
Jonathan Foster <fostertheweb@gmail.com>
2023-10-08 22:12:37 -0400

    feeling safe

--- src/lib/components/DeviceSelector.svelte
+++ src/lib/components/DeviceSelector.svelte
@@ -2,7 +2,7 @@
 	import { createRadioGroup, melt } from '@melt-ui/svelte';
 	import type { Device } from '@fostertheweb/spotify-web-api-ts-sdk';
 
-	export let devices: Array<Device>;
+	export let devices: Array<Device> | undefined;
 
 	const {
 		elements: { root, item },
@@ -16,7 +16,7 @@
 	use:melt={$root}
 	class="flex flex-col gap-3 data-[orientation=horizontal]:flex-row"
 	aria-label="View density">
-	{#if devices.length === 0}
+	{#if !devices || devices.length === 0}
 		<div
 			class="flex flex-col items-center justify-center gap-4 rounded bg-stone-100 p-4 dark:bg-stone-700 dark:text-stone-400">
 			<div>No active devices found. Open the Spotify app and return to crowdq.fm</div>
@@ -25,28 +25,28 @@
 				class="rounded-full bg-green-500 px-3 py-2 font-general font-medium text-white"
 				>Open Spotify</a>
 		</div>
-	{/if}
-
-	{#each devices as device}
-		{#if device.id}
-			<div class="flex items-center gap-3">
-				<button
-					use:melt={$item(device.id)}
-					class="hover:bg-magnum-100 grid h-6 w-6 cursor-default place-items-center rounded-full bg-white
+	{:else}
+		{#each devices as device}
+			{#if device.id}
+				<div class="flex items-center gap-3">
+					<button
+						use:melt={$item(device.id)}
+						class="hover:bg-magnum-100 grid h-6 w-6 cursor-default place-items-center rounded-full bg-white
       shadow-sm"
-					id={device.id}
-					aria-labelledby="{device.id}-label">
-					{#if $isChecked(device.id)}
-						<div class="bg-magnum-500 h-3 w-3 rounded-full" />
-					{/if}
-				</button>
-				<label
-					class="text-magnum-900 font-medium capitalize leading-none"
-					for={device.id}
-					id="{device.id}-label">
-					{device.name}
-				</label>
-			</div>
-		{/if}
-	{/each}
+						id={device.id}
+						aria-labelledby="{device.id}-label">
+						{#if $isChecked(device.id)}
+							<div class="bg-magnum-500 h-3 w-3 rounded-full" />
+						{/if}
+					</button>
+					<label
+						class="text-magnum-900 font-medium capitalize leading-none"
+						for={device.id}
+						id="{device.id}-label">
+						{device.name}
+					</label>
+				</div>
+			{/if}
+		{/each}
+	{/if}
 </div>
