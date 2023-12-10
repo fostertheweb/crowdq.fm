fd9f8ae3c134cc9eeea2f9ae64f94b1574c63134
Jonathan Foster <fostertheweb@gmail.com>
2023-12-09 19:33:20 -0500

    dim spotify dark shadow

--- src/lib/components/ContinueButton.svelte
+++ src/lib/components/ContinueButton.svelte
@@ -4,7 +4,7 @@
 
 <a
 	href="/lobby"
-	class="leading flex w-full items-center justify-center gap-2 rounded-md bg-[#1cd760] px-6 py-3 text-center font-readex-pro text-base font-semibold text-white shadow-sm filter transition ease-linear hover:shadow-md hover:brightness-105">
+	class="leading flex w-full items-center justify-center gap-2 rounded-md bg-[#1cd760] px-6 py-3 text-center font-readex-pro text-base font-semibold text-white shadow-sm filter transition ease-linear hover:shadow-md hover:brightness-105 dark:shadow-[#1cd760]/20">
 	<i class="fa-brands fa-spotify" />
 	<span>Continue as {name}</span>
 </a>
