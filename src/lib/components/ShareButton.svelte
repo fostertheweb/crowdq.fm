db8c23f59fdc295f1af10b100d26f943aed58938
Jonathan Foster <fostertheweb@gmail.com>
2024-03-05 01:08:01 -0500

    bits-ui tooltip

--- src/lib/components/ShareButton.svelte
+++ src/lib/components/ShareButton.svelte
@@ -1,22 +1,9 @@
 <script lang="ts">
 	import { browser } from '$app/environment';
 	import IconShare from '$lib/components/icons/IconShare.svelte';
-	import { createTooltip, melt } from '@melt-ui/svelte';
+	import { Tooltip } from 'bits-ui';
 	import { fade } from 'svelte/transition';
 	import { addToast } from './Toaster.svelte';
-
-	const {
-		elements: { trigger, content, arrow },
-		states: { open }
-	} = createTooltip({
-		positioning: {
-			placement: 'left'
-		},
-		openDelay: 0,
-		closeDelay: 0,
-		closeOnPointerDown: true,
-		forceVisible: true
-	});
 
 	async function share() {
 		if (browser) {
@@ -42,19 +29,21 @@
 	}
 </script>
 
-{#if $open}
-	<div
-		use:melt={$content}
-		transition:fade={{ duration: 100 }}
+<Tooltip.Root openDelay={0} closeDelay={0} closeOnPointerDown>
+	<Tooltip.Trigger>
+		<button
+			aria-label="Copy invite link"
+			on:click={share}
+			class="flex h-8 w-8 items-center justify-center gap-2 rounded-full bg-stone-200/60 text-sm text-stone-500 hover:bg-stone-200 hover:text-stone-600 dark:bg-stone-700 dark:text-stone-300 dark:hover:bg-stone-600 dark:hover:text-stone-200"
+			><IconShare /></button>
+	</Tooltip.Trigger>
+	<Tooltip.Content
+		side="left"
+		sideOffset={4}
+		transition={fade}
+		transitionConfig={{ duration: 100 }}
 		class="z-20 flex w-fit items-center gap-2 rounded-lg border border-stone-200 bg-stone-100 px-3 py-2 text-sm text-stone-600 shadow-md shadow-black/5 backdrop-blur-sm dark:border-stone-800 dark:bg-stone-900 dark:text-stone-50">
-		<div use:melt={$arrow} class="border-l border-t border-stone-200 dark:border-stone-800" />
+		<Tooltip.Arrow class="border-l border-t border-stone-200 dark:border-stone-800" />
 		<p class="text-xs font-medium">Copy invite link</p>
-	</div>
-{/if}
-
-<button
-	aria-label="Copy invite link"
-	use:melt={$trigger}
-	on:click={share}
-	class="flex h-8 w-8 items-center justify-center gap-2 rounded-full bg-stone-200/60 text-sm text-stone-500 hover:bg-stone-200 hover:text-stone-600 dark:bg-stone-700 dark:text-stone-300 dark:hover:bg-stone-600 dark:hover:text-stone-200"
-	><IconShare /></button>
+	</Tooltip.Content>
+</Tooltip.Root>
